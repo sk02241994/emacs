9335b0d9022ca12b1a9b747ea693a8ab90412b84
sk02241994 <skaradkar57@gmail.com>
2024-01-05 07:56:29 +0530

    Adding theme and which key plugin

--- init.el
+++ init.el
@@ -7,3 +7,37 @@
 (setq display-line-numbers-type 'relative)
 (setq font-lock-maximum-decoration t)
 (setq make-backup-files nil)
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
+
+;; Set up package.el to work with MELPA
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+(package-refresh-contents)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("871b064b53235facde040f6bdfa28d03d9f4b966d8ce28fb1725313731a2bcc8" default))
+ '(package-selected-packages '(which-key gruvbox-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(use-package gruvbox-theme
+:ensure t
+:config
+(load-theme 'gruvbox-dark-soft t))
+
+(require 'which-key)    
+(which-key-mode)
+(which-key-setup-side-window-bottom)
